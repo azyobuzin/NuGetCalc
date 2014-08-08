423d7c8b0e1a30f7360a0c4f0aaddf1593a63f1d
azyobuzin <azyobuzin@users.sourceforge.jp>
2014-08-08 22:22:45 +0900

    v1.0

--- NuGetCalc/Properties/AssemblyInfo.cs
+++ NuGetCalc/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
 [assembly: AssemblyTitle("NuGetCalc")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Tool to find which assembly NuGet adds to your project ")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NuGetCalc")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © 2014 azyobuzin")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
